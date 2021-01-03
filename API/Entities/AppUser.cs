db955fc930a7793459e7dffbd2d515b1a1ff985b
Usman Zaheer <uz@trainaway.fit>
2021-01-03 19:40:35 +0500

    Added basic authentication and authorization with JWT

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,5 +4,7 @@ namespace API.Entities
     {
         public int ID { get; set; }
         public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
