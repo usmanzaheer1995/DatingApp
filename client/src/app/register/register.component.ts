6269ee7c6b0d60f8b78f65ad8b6f6b6d03ce8ae4
Usman Zaheer <usmanzaheer1995@gmail.com>
2021-01-10 18:50:56 +0500

    Added user login and registration

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output("cancel") cancelRegister = new EventEmitter();
+
+  model: any = {};
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    this.accountService.register(this.model)
+      .subscribe(
+        (response) => {
+          console.log(response);
+          this.cancel();
+        }
+      );
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+
+}
